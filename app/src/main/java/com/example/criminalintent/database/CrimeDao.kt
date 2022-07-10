6289cc243211f6ab253f3e0c7a13d29e4ed923a7
andre <andre.pires73@outlook.com>
2022-07-10 07:04:32 -0300

    "Databases and the Room library" chapter implementations done.

---
+++ app/src/main/java/com/example/criminalintent/database/CrimeDao.kt
@@ -0,0 +1,19 @@
+package com.example.criminalintent.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Query
+import com.example.criminalintent.Crime
+import java.util.*
+
+// data access object
+
+@Dao
+interface CrimeDao {
+
+    @Query("SELECT * FROM crime")
+    fun getCrimes(): LiveData<List<Crime>>
+
+    @Query("SELECT * FROM crime WHERE id=(:id)")
+    fun getCrime(id: UUID): LiveData<Crime?>
+}
